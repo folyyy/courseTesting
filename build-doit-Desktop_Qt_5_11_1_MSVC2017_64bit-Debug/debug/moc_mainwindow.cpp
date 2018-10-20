528e01e676c81e1c542175a40b12d9e511c1e4cc
folyyy <folyhideout@gmail.com>
2018-10-20 12:15:52 +0300

    integrating to laptop

--- build-doit-Desktop_Qt_5_11_1_MSVC2017_64bit-Debug/debug/moc_mainwindow.cpp
+++ build-doit-Desktop_Qt_5_11_1_MSVC2017_64bit-Debug/debug/moc_mainwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[8];
-    char stringdata0[167];
+    QByteArrayData data[9];
+    char stringdata0[191];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,17 +32,19 @@ struct qt_meta_stringdata_MainWindow_t {
 static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
     {
 QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
-QT_MOC_LITERAL(1, 11, 26), // "on_buildDictionary_clicked"
-QT_MOC_LITERAL(2, 38, 0), // ""
-QT_MOC_LITERAL(3, 39, 30), // "on_putDictionaryInFile_clicked"
-QT_MOC_LITERAL(4, 70, 27), // "on_outputDictionary_clicked"
-QT_MOC_LITERAL(5, 98, 21), // "on_addElement_clicked"
-QT_MOC_LITERAL(6, 120, 19), // "on_findWord_clicked"
-QT_MOC_LITERAL(7, 140, 26) // "on_increaseCounter_clicked"
+QT_MOC_LITERAL(1, 11, 23), // "on_browseButton_clicked"
+QT_MOC_LITERAL(2, 35, 0), // ""
+QT_MOC_LITERAL(3, 36, 26), // "on_buildDictionary_clicked"
+QT_MOC_LITERAL(4, 63, 30), // "on_putDictionaryInFile_clicked"
+QT_MOC_LITERAL(5, 94, 27), // "on_outputDictionary_clicked"
+QT_MOC_LITERAL(6, 122, 21), // "on_addElement_clicked"
+QT_MOC_LITERAL(7, 144, 19), // "on_findWord_clicked"
+QT_MOC_LITERAL(8, 164, 26) // "on_increaseCounter_clicked"
 
     },
-    "MainWindow\0on_buildDictionary_clicked\0"
-    "\0on_putDictionaryInFile_clicked\0"
+    "MainWindow\0on_browseButton_clicked\0\0"
+    "on_buildDictionary_clicked\0"
+    "on_putDictionaryInFile_clicked\0"
     "on_outputDictionary_clicked\0"
     "on_addElement_clicked\0on_findWord_clicked\0"
     "on_increaseCounter_clicked"
@@ -55,7 +57,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       6,   14, // methods
+       7,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -63,14 +65,16 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   44,    2, 0x08 /* Private */,
-       3,    0,   45,    2, 0x08 /* Private */,
-       4,    0,   46,    2, 0x08 /* Private */,
-       5,    0,   47,    2, 0x08 /* Private */,
-       6,    0,   48,    2, 0x08 /* Private */,
-       7,    0,   49,    2, 0x08 /* Private */,
+       1,    0,   49,    2, 0x08 /* Private */,
+       3,    0,   50,    2, 0x08 /* Private */,
+       4,    0,   51,    2, 0x08 /* Private */,
+       5,    0,   52,    2, 0x08 /* Private */,
+       6,    0,   53,    2, 0x08 /* Private */,
+       7,    0,   54,    2, 0x08 /* Private */,
+       8,    0,   55,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -87,12 +91,13 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         MainWindow *_t = static_cast<MainWindow *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->on_buildDictionary_clicked(); break;
-        case 1: _t->on_putDictionaryInFile_clicked(); break;
-        case 2: _t->on_outputDictionary_clicked(); break;
-        case 3: _t->on_addElement_clicked(); break;
-        case 4: _t->on_findWord_clicked(); break;
-        case 5: _t->on_increaseCounter_clicked(); break;
+        case 0: _t->on_browseButton_clicked(); break;
+        case 1: _t->on_buildDictionary_clicked(); break;
+        case 2: _t->on_putDictionaryInFile_clicked(); break;
+        case 3: _t->on_outputDictionary_clicked(); break;
+        case 4: _t->on_addElement_clicked(); break;
+        case 5: _t->on_findWord_clicked(); break;
+        case 6: _t->on_increaseCounter_clicked(); break;
         default: ;
         }
     }
@@ -126,13 +131,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 6)
+        if (_id < 7)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 6;
+        _id -= 7;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 6)
+        if (_id < 7)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 6;
+        _id -= 7;
     }
     return _id;
 }
--- build-doit-Desktop_Qt_5_11_1_MSVC2017_64bit-Debug/ui_mainwindow.h
+++ build-doit-Desktop_Qt_5_11_1_MSVC2017_64bit-Debug/ui_mainwindow.h
@@ -11,12 +11,14 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QLineEdit>
+#include <QtWidgets/QListWidget>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
-#include <QtWidgets/QStatusBar>
-#include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -25,56 +27,238 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralWidget;
+    QGridLayout *gridLayout_4;
+    QHBoxLayout *horizontalLayout_2;
+    QVBoxLayout *verticalLayout_3;
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QPushButton *browseButton;
     QPushButton *buildDictionary;
+    QListWidget *listWidget;
+    QGridLayout *gridLayout_3;
+    QPushButton *findWord;
+    QPushButton *addElement;
+    QPushButton *increaseCounter;
     QPushButton *putDictionaryInFile;
     QPushButton *outputDictionary;
-    QPushButton *addElement;
-    QPushButton *findWord;
-    QPushButton *increaseCounter;
-    QLineEdit *lineEdit;
-    QMenuBar *menuBar;
-    QToolBar *mainToolBar;
-    QStatusBar *statusBar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(560, 381);
+        MainWindow->resize(745, 568);
+        MainWindow->setStyleSheet(QStringLiteral("QTextEdit{background-color: white; }"));
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        buildDictionary = new QPushButton(centralWidget);
-        buildDictionary->setObjectName(QStringLiteral("buildDictionary"));
-        buildDictionary->setGeometry(QRect(190, 10, 201, 31));
-        putDictionaryInFile = new QPushButton(centralWidget);
-        putDictionaryInFile->setObjectName(QStringLiteral("putDictionaryInFile"));
-        putDictionaryInFile->setGeometry(QRect(190, 210, 201, 31));
-        outputDictionary = new QPushButton(centralWidget);
-        outputDictionary->setObjectName(QStringLiteral("outputDictionary"));
-        outputDictionary->setGeometry(QRect(190, 50, 201, 31));
-        addElement = new QPushButton(centralWidget);
-        addElement->setObjectName(QStringLiteral("addElement"));
-        addElement->setGeometry(QRect(190, 90, 201, 31));
-        findWord = new QPushButton(centralWidget);
-        findWord->setObjectName(QStringLiteral("findWord"));
-        findWord->setGeometry(QRect(190, 130, 201, 31));
-        increaseCounter = new QPushButton(centralWidget);
-        increaseCounter->setObjectName(QStringLiteral("increaseCounter"));
-        increaseCounter->setGeometry(QRect(190, 170, 201, 31));
+        centralWidget->setStyleSheet(QStringLiteral("background-color: #FFF8DC;"));
+        gridLayout_4 = new QGridLayout(centralWidget);
+        gridLayout_4->setSpacing(6);
+        gridLayout_4->setContentsMargins(11, 11, 11, 11);
+        gridLayout_4->setObjectName(QStringLiteral("gridLayout_4"));
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setSpacing(6);
+        horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
+        verticalLayout_3 = new QVBoxLayout();
+        verticalLayout_3->setSpacing(6);
+        verticalLayout_3->setObjectName(QStringLiteral("verticalLayout_3"));
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+        QFont font;
+        font.setFamily(QStringLiteral("Arial"));
+        font.setBold(true);
+        font.setItalic(true);
+        font.setWeight(75);
+        label->setFont(font);
+
+        verticalLayout_3->addWidget(label);
+
         lineEdit = new QLineEdit(centralWidget);
         lineEdit->setObjectName(QStringLiteral("lineEdit"));
-        lineEdit->setGeometry(QRect(190, 250, 201, 22));
+
+        verticalLayout_3->addWidget(lineEdit);
+
+        browseButton = new QPushButton(centralWidget);
+        browseButton->setObjectName(QStringLiteral("browseButton"));
+        browseButton->setMinimumSize(QSize(460, 0));
+        browseButton->setMaximumSize(QSize(500, 16777215));
+        QFont font1;
+        font1.setFamily(QStringLiteral("Arial"));
+        font1.setPointSize(10);
+        font1.setBold(true);
+        font1.setItalic(true);
+        font1.setWeight(75);
+        browseButton->setFont(font1);
+        browseButton->setStyleSheet(QLatin1String("  display: inline-block;\n"
+"  color: black;\n"
+"  text-decoration: none;\n"
+"  padding: .5em 2em;\n"
+"   border-width: 1px ;\n"
+"  border-style: solid ;\n"
+"  border-color:  #000000 #000000 #000000;\n"
+"  border-radius: 3px;\n"
+"  background: #F5DEB3;\n"
+"  transition: 0.2s;"));
+
+        verticalLayout_3->addWidget(browseButton);
+
+        buildDictionary = new QPushButton(centralWidget);
+        buildDictionary->setObjectName(QStringLiteral("buildDictionary"));
+        buildDictionary->setMinimumSize(QSize(460, 0));
+        buildDictionary->setMaximumSize(QSize(500, 16777215));
+        buildDictionary->setFont(font1);
+        buildDictionary->setStyleSheet(QLatin1String("  display: inline-block;\n"
+"  color: black;\n"
+"  text-decoration: none;\n"
+"  padding: .5em 2em;\n"
+"   border-width: 1px ;\n"
+"  border-style: solid ;\n"
+"  border-color:  #000000 #000000 #000000;\n"
+"  border-radius: 3px;\n"
+"  background: #F5DEB3;\n"
+"  transition: 0.2s;"));
+
+        verticalLayout_3->addWidget(buildDictionary);
+
+        listWidget = new QListWidget(centralWidget);
+        listWidget->setObjectName(QStringLiteral("listWidget"));
+        listWidget->setMinimumSize(QSize(460, 400));
+        listWidget->setMaximumSize(QSize(450, 400));
+        listWidget->setAutoFillBackground(false);
+        listWidget->setStyleSheet(QStringLiteral("background-color: white;"));
+
+        verticalLayout_3->addWidget(listWidget);
+
+
+        horizontalLayout_2->addLayout(verticalLayout_3);
+
+        gridLayout_3 = new QGridLayout();
+        gridLayout_3->setSpacing(6);
+        gridLayout_3->setObjectName(QStringLiteral("gridLayout_3"));
+        findWord = new QPushButton(centralWidget);
+        findWord->setObjectName(QStringLiteral("findWord"));
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy.setHorizontalStretch(200);
+        sizePolicy.setVerticalStretch(50);
+        sizePolicy.setHeightForWidth(findWord->sizePolicy().hasHeightForWidth());
+        findWord->setSizePolicy(sizePolicy);
+        findWord->setMinimumSize(QSize(250, 50));
+        findWord->setMaximumSize(QSize(250, 50));
+        QFont font2;
+        font2.setFamily(QStringLiteral("Arial"));
+        font2.setPointSize(9);
+        font2.setBold(true);
+        font2.setItalic(true);
+        font2.setWeight(75);
+        findWord->setFont(font2);
+        findWord->setStyleSheet(QLatin1String("  display: inline-block;\n"
+"  color: black;\n"
+"  text-decoration: none;\n"
+"  padding: .5em 2em;\n"
+"   border-width: 1px ;\n"
+"  border-style: solid ;\n"
+"  border-color:  #000000 #000000 #000000;\n"
+"  border-radius: 3px;\n"
+"  background: #F5DEB3;\n"
+"  transition: 0.2s;"));
+
+        gridLayout_3->addWidget(findWord, 3, 2, 1, 1);
+
+        addElement = new QPushButton(centralWidget);
+        addElement->setObjectName(QStringLiteral("addElement"));
+        QSizePolicy sizePolicy1(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(addElement->sizePolicy().hasHeightForWidth());
+        addElement->setSizePolicy(sizePolicy1);
+        addElement->setMinimumSize(QSize(250, 50));
+        addElement->setMaximumSize(QSize(250, 50));
+        addElement->setFont(font2);
+        addElement->setStyleSheet(QLatin1String("  display: inline-block;\n"
+"  color: black;\n"
+"  text-decoration: none;\n"
+"  padding: .5em 2em;\n"
+"   border-width: 1px ;\n"
+"  border-style: solid ;\n"
+"  border-color:  #000000 #000000 #000000;\n"
+"  border-radius: 3px;\n"
+"  background: #F5DEB3;\n"
+"  transition: 0.2s;"));
+
+        gridLayout_3->addWidget(addElement, 1, 2, 1, 1);
+
+        increaseCounter = new QPushButton(centralWidget);
+        increaseCounter->setObjectName(QStringLiteral("increaseCounter"));
+        sizePolicy1.setHeightForWidth(increaseCounter->sizePolicy().hasHeightForWidth());
+        increaseCounter->setSizePolicy(sizePolicy1);
+        increaseCounter->setMinimumSize(QSize(250, 50));
+        increaseCounter->setMaximumSize(QSize(250, 50));
+        QFont font3;
+        font3.setFamily(QStringLiteral("Arial"));
+        font3.setPointSize(8);
+        font3.setBold(true);
+        font3.setItalic(true);
+        font3.setWeight(75);
+        increaseCounter->setFont(font3);
+        increaseCounter->setStyleSheet(QLatin1String("  display: inline-block;\n"
+"  color: black;\n"
+"  text-decoration: none;\n"
+"  padding: .5em 2em;\n"
+"   border-width: 1px ;\n"
+"  border-style: solid ;\n"
+"  border-color:  #000000 #000000 #000000;\n"
+"  border-radius: 3px;\n"
+"  background: #F5DEB3;\n"
+"  transition: 0.2s;"));
+
+        gridLayout_3->addWidget(increaseCounter, 2, 2, 1, 1);
+
+        putDictionaryInFile = new QPushButton(centralWidget);
+        putDictionaryInFile->setObjectName(QStringLiteral("putDictionaryInFile"));
+        sizePolicy1.setHeightForWidth(putDictionaryInFile->sizePolicy().hasHeightForWidth());
+        putDictionaryInFile->setSizePolicy(sizePolicy1);
+        putDictionaryInFile->setMinimumSize(QSize(250, 50));
+        putDictionaryInFile->setMaximumSize(QSize(250, 50));
+        putDictionaryInFile->setFont(font2);
+        putDictionaryInFile->setStyleSheet(QLatin1String("  display: inline-block;\n"
+"  color: black;\n"
+"  text-decoration: none;\n"
+"  padding: .5em 2em;\n"
+"   border-width: 1px ;\n"
+"  border-style: solid ;\n"
+"  border-color:  #000000 #000000 #000000;\n"
+"  border-radius: 3px;\n"
+"  background: #F5DEB3;\n"
+"  transition: 0.2s;"));
+
+        gridLayout_3->addWidget(putDictionaryInFile, 4, 2, 1, 1);
+
+        outputDictionary = new QPushButton(centralWidget);
+        outputDictionary->setObjectName(QStringLiteral("outputDictionary"));
+        sizePolicy1.setHeightForWidth(outputDictionary->sizePolicy().hasHeightForWidth());
+        outputDictionary->setSizePolicy(sizePolicy1);
+        outputDictionary->setMinimumSize(QSize(250, 50));
+        outputDictionary->setMaximumSize(QSize(250, 50));
+        outputDictionary->setFont(font2);
+        outputDictionary->setStyleSheet(QLatin1String("  display: inline-block;\n"
+"  color: black;\n"
+"  text-decoration: none;\n"
+"  padding: .5em 2em;\n"
+"   border-width: 1px ;\n"
+"  border-style: solid ;\n"
+"  border-color:  #000000 #000000 #000000;\n"
+"  border-radius: 3px;\n"
+"  background: #F5DEB3;\n"
+"  transition: 0.2s;"));
+
+        gridLayout_3->addWidget(outputDictionary, 0, 2, 1, 1);
+
+
+        horizontalLayout_2->addLayout(gridLayout_3);
+
+
+        gridLayout_4->addLayout(horizontalLayout_2, 0, 0, 1, 1);
+
         MainWindow->setCentralWidget(centralWidget);
-        menuBar = new QMenuBar(MainWindow);
-        menuBar->setObjectName(QStringLiteral("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 560, 20));
-        MainWindow->setMenuBar(menuBar);
-        mainToolBar = new QToolBar(MainWindow);
-        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
-        statusBar = new QStatusBar(MainWindow);
-        statusBar->setObjectName(QStringLiteral("statusBar"));
-        MainWindow->setStatusBar(statusBar);
 
         retranslateUi(MainWindow);
 
@@ -83,13 +267,24 @@ public:
 
     void retranslateUi(QMainWindow *MainWindow)
     {
-        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
-        buildDictionary->setText(QApplication::translate("MainWindow", "\320\241\320\276\320\267\320\264\320\260\321\202\321\214 \321\201\320\273\320\276\320\262\320\260\321\200\321\214 \320\270\320\267 \321\204\320\260\320\271\320\273\320\260", nullptr));
-        putDictionaryInFile->setText(QApplication::translate("MainWindow", "\320\227\320\260\320\277\320\270\321\201\320\260\321\202\321\214 \321\201\320\273\320\276\320\262\320\260\321\200\321\214 \320\262 \321\204\320\260\320\271\320\273", nullptr));
-        outputDictionary->setText(QApplication::translate("MainWindow", "\320\222\321\213\320\262\320\265\321\201\321\202\320\270 \321\201\320\273\320\276\320\262\320\260\321\200\321\214 \320\275\320\260 \321\215\320\272\321\200\320\260\320\275", nullptr));
-        addElement->setText(QApplication::translate("MainWindow", "\320\224\320\276\320\261\320\260\320\262\320\270\321\202\321\214 \321\215\320\273\320\265\320\274\320\265\320\275\321\202 \320\262 \321\201\320\273\320\276\320\262\320\260\321\200\321\214", nullptr));
-        findWord->setText(QApplication::translate("MainWindow", "\320\235\320\260\320\271\321\202\320\270 \321\201\320\273\320\276\320\262\320\276 \320\262 \321\201\320\273\320\276\320\262\320\260\321\200\320\265", nullptr));
-        increaseCounter->setText(QApplication::translate("MainWindow", "\320\243\320\262\320\265\320\273\320\270\321\207\320\265\320\275\320\270\320\265 \320\267\320\275\320\260\321\207\320\265\320\275\320\270\321\217 \321\201\321\207\320\265\321\202\321\207\320\270\320\272\320\260 \321\201\320\273\320\276\320\262\320\260", nullptr));
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "\320\247\320\260\321\201\321\202\320\276\321\202\320\275\321\213\320\271 \321\201\320\273\320\276\320\262\320\260\321\200\321\214", nullptr));
+        label->setText(QApplication::translate("MainWindow", "\320\242\320\265\321\200\320\274\320\270\320\275\320\260\320\273", nullptr));
+#ifndef QT_NO_TOOLTIP
+        browseButton->setToolTip(QApplication::translate("MainWindow", "<html><head/><body><p/></body></html>", nullptr));
+#endif // QT_NO_TOOLTIP
+#ifndef QT_NO_WHATSTHIS
+        browseButton->setWhatsThis(QApplication::translate("MainWindow", "<html><head/><body><p>\320\232\320\273\320\270\320\272\320\275\320\270\321\202\320\265 \320\264\320\273\321\217 \320\262\321\213\320\261\320\276\321\200\320\260 \321\204\320\260\320\271\320\273\320\260.</p></body></html>", nullptr));
+#endif // QT_NO_WHATSTHIS
+        browseButton->setText(QApplication::translate("MainWindow", "\320\236\320\261\320\267\320\276\321\200", nullptr));
+#ifndef QT_NO_WHATSTHIS
+        buildDictionary->setWhatsThis(QApplication::translate("MainWindow", "<html><head/><body><p>\320\232\320\273\320\270\320\272\320\275\320\270\321\202\320\265 \321\207\321\202\320\276\320\261\321\213 \321\201\320\276\320\267\320\264\320\260\321\202\321\214 \321\201\320\273\320\276\320\262\320\260\321\200\321\214.<br/></p></body></html>", nullptr));
+#endif // QT_NO_WHATSTHIS
+        buildDictionary->setText(QApplication::translate("MainWindow", "\320\241\320\276\320\267\320\264\320\260\321\202\321\214 \321\201\320\273\320\276\320\262\320\260\321\200\321\214", nullptr));
+        findWord->setText(QApplication::translate("MainWindow", "\320\237\320\276\320\270\321\201\320\272 \320\267\320\260\320\264\320\260\320\275\320\275\320\276\320\263\320\276 \321\201\320\273\320\276\320\262\320\260", nullptr));
+        addElement->setText(QApplication::translate("MainWindow", "\320\224\320\276\320\261\320\260\320\262\320\273\320\265\320\275\320\270\320\265 \321\201\320\273\320\276\320\262\320\260", nullptr));
+        increaseCounter->setText(QApplication::translate("MainWindow", "\320\243\320\262\320\265\320\273\320\270\321\207\320\265\320\275\320\270\320\265 \321\201\321\207\320\265\321\202\321\207\320\270\320\272\320\260 \321\201\320\273\320\276\320\262\320\260", nullptr));
+        putDictionaryInFile->setText(QApplication::translate("MainWindow", "\320\227\320\260\320\277\320\270\321\201\321\214 \320\262 \321\204\320\260\320\271\320\273", nullptr));
+        outputDictionary->setText(QApplication::translate("MainWindow", "\320\222\321\213\320\262\320\276\320\264 \321\201\320\273\320\276\320\262\320\260\321\200\321\217 \320\275\320\260 \321\215\320\272\321\200\320\260\320\275", nullptr));
     } // retranslateUi
 
 };
